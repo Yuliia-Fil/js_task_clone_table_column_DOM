201b3d9dd1eb8093ef09ad47d0a6d985d9e0d096
Yulia Fil <filyulia.yf@gmail.com>
2025-05-01 22:17:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const trows = document.querySelectorAll('tr');
+
+for (const tr of trows) {
+  const secondChild = tr.querySelector('tr > :nth-child(2)');
+  const cloneSecondChild = document.createElement(`${secondChild.tagName}`);
+
+  cloneSecondChild.textContent = secondChild.textContent;
+
+  const fourthChild = tr.querySelector('tr > :nth-child(4)');
+
+  fourthChild.after(cloneSecondChild);
+}
